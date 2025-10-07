6cd0053d3523278956f1fd2fbbe0d712358b32a4
Sejal Jagtap <sejaljagtap21@gmail.com>
2025-10-07 14:46:44 -0400

    Queries and result images updated

--- sql_queries.sql
+++ sql_queries.sql
@@ -7,7 +7,7 @@ FROM university_rankings
 LIMIT 5;
 
 /* Count total universities */
-SELECT count(*) as number_of_universities
+SELECT count(distinct(institution)) as number_of_universities
 FROM university_rankings;
 
 /* Count total universities */
@@ -63,40 +63,34 @@ ORDER BY universities_in_top_100 DESC;
 
 /* ---------------------------------------- CRUD Operations ------------------------------------------------------- */
 
-/* Add a new university to the database */
+
 INSERT INTO university_rankings (institution, country, world_rank, score, year)
 VALUES ('Duke Tech', 'USA', 350, 60.5, 2014);
 
-/* Check if the new university has been added */
 SELECT institution, country, world_rank, score, year
 FROM university_rankings
 WHERE institution = 'Duke Tech';
 
-/* Number of universities from Japan that show up in the global top 200 in 2013 */
 SELECT COUNT(DISTINCT institution ) AS top_200_japan_2013
 FROM university_rankings
 WHERE country = 'Japan'
 AND world_rank <= 200
 AND year = 2013;
 
-/* Update the score for University of Oxford in 2014 by +1.2 points    */
 UPDATE university_rankings
 SET score = score + 1.2
 WHERE institution  = 'University of Oxford'
 AND year = 2014;
 
-/* Check if the score was updated */
 SELECT score,* 
 FROM university_rankings 
 WHERE institution  = 'University of Oxford'
 AND year = 2014;
 
-/* Delete universities with a score below 45 in 2015 */
 DELETE FROM university_rankings
 WHERE year = 2015
 AND score < 45;
 
-/* Check if the records were deleted, result should give 0 records */
 SELECT * 
 FROM university_rankings 
 WHERE year = 2015 
